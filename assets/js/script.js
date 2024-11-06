5ca16ace82e6bb3566dd5388b9e99f8ca25707ab
Pedro Henrique Souza Lima <pedrohenriquesolt@gmail.com>
2024-11-06 16:36:26 -0300

    atualização
     projeto criado a partir de programação a objetos, utilizando classes

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+let log = new Log(document.querySelector('.log'));
+
+let char = new Knight('Pedro');
+let monster = new BigMonster();
+
+
+const stage = new Stage(
+    char,
+    monster,
+    document.querySelector('#char'),
+    document.querySelector('#monster'),
+    log
+);
+
+stage.start();
+
+
